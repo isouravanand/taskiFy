da7046c1a89eb3b0718bb8d0987f9ff6a317e4f4
isouravanand <heysouravanand@gmail.com>
2025-03-07 22:29:39 +0530

    added more button

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Task from'./components/Task' ;
+import { KeyboardAvoidingView } from 'react-native';
+import { TextInput } from 'react-native-web';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      {/*more and addnew button */}
+      <View style={styles.option}>
+  <TouchableOpacity style={styles.icon}>
+    <Text style={styles.iconText}>☰</Text>
+  </TouchableOpacity>
+  <Text style={styles.more}>More</Text>
+
+</View>
+
       {/* Today's tasks */}
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
@@ -23,13 +34,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED',
+    backgroundColor: '#fbb03b'
+    // backgroundColor: '#E8EAED',
   },
+  option: {
+    backgroundColor: '#E8EAED',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon :{
+    padding: 10,
+  },
+  iconText: {
+    fontSize: 20, // Adjust size as needed
+  },
+  more: {
+    marginLeft: 8, // Add spacing between icon and text
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  // addNewButton: {
+  //   backgroundColor: '#55BCF6',
+  //   paddingVertical: 6,
+  //   paddingHorizontal: 12,
+  //   borderRadius: 6,
+  //   alignItems: 'flex-end',
+  // },
   tasksWrapper: {  
-    paddingTop: 80,
+    // backgroundColor: '#fbb03b',
+    paddingTop: 50,
     paddingHorizontal: 20,
   },
-  sectionTitle: {  
+  sectionTitle: { 
+   color: '#fff', 
    fontSize: 24,
    fontWeight: 'bold',
   },
